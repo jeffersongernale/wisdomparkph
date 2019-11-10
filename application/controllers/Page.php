b11a5fc0547e7dd3a4d14ea14d9ad20780e70750
jeffersongernale <jepgernale@gmail.com>
2019-11-10 16:14:06 +0800

    first commit

---
+++ application/controllers/Page.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Page extends CI_Controller {
+
+
+	
+
+	public function index()
+	{
+		// $this->load->view('login');
+		$this->load->view('index');
+
+	}
+
+
+}
---
+++ application/views/login.php
@@ -0,0 +1,42 @@
+<html>
+
+<head>
+
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+	<link rel="stylesheet" href="asset/css/custom.css">
+	<link rel="stylesheet" href="asset/css/login.css">
+</head>
+<body class="cont">
+
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-sm-3"></div>
+			<div class="col-lg-4 col-sm-6 p-5 text-center">
+				<div class="border login_container p-5">
+					<img src="asset/images/LOGO/LOGO.png" class="login_logo">
+					<h2>WISDOM PARK</h2> <br>
+					<h4>Administrator Panel</h4>
+					<br>
+					<div class="text-left">
+					<b>Username:</b>
+					<input type="text" class="form-control" id="txt_username" name="txt_username" placeholder="Input Username"> 
+					<br>
+					<b>Password:</b>
+					<input type="password" class="form-control" id="txt_password" name="txt_password"  placeholder="Input Password">
+					<br>
+					</div>
+					<button class="btn btn-md btn-info font-weight-bold form-control mb-3">LOGIN</button>
+					<button class="btn btn-md btn-danger font-weight-bold form-control">CANCEL</button>
+				</div>
+			</div>
+			<div class="col-lg-4  col-sm-3"></div>
+		</div>
+	</div>
+
+
+
+	<script src="asset/js/jquery-3.3.1.min.js"></script>
+  	<script src="asset/js/bootstrap.min.js"></script>
+</body>
+</html>
