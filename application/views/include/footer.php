272ec1f75ef678823336d45aec644c20b4c6748d
nyeperson <jepgernale@gmail.com>
2020-01-03 20:24:30 +0800

    [SOCIAL MEDIA]

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -21,10 +21,10 @@
               </div>
               <div class="col-md-3">
                 <h2 class="footer-heading mb-4">Follow Us</h2>
-                <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
-                <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
-                <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-                <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+                <a href="https://www.facebook.com/wisdomparkphilippines/" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                <a href="https://twitter.com/WisdomParkph" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                <a href="https://www.youtube.com/channel/UC2jUjrw8JydeqRJIKd6U-wA?view_as=subscriber" class="pl-3 pr-3"><span class="icon-youtube"></span></a>
+                <!-- <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a> -->
               </div>
             </div>
           </div>
