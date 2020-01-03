9fe67d20ffad91d8ab85fec11e05e93af1b8eec6
nyeperson <jepgernale@gmail.com>
2020-01-03 19:33:26 +0800

    [SIDEBAR]

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -5,7 +5,7 @@
       <span style="color:gray;font-size:12px">Admin Panel</span>
     </div>
     <div class="list-group list-group-flush">
-      <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fa fa-tachometer-alt"></i> &nbsp;Dashboard</a>
+      <!-- <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fa fa-tachometer-alt"></i> &nbsp;Dashboard</a> -->
       <a href="admin-details" class="list-group-item list-group-item-action bg-light"><i class="fa fa-info"></i> &nbsp;Website Details</a>
       <a href="admin-gallery" class="list-group-item list-group-item-action bg-light"><i class="fa fa-photo-video"></i> &nbsp;Gallery</a>
       <a href="admin-events" class="list-group-item list-group-item-action bg-light"><i class="fa fa-calendar-check"></i> &nbsp;Events</a>
