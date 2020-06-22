82e1b5c8a38f8239cb645eff0f86eb92c61d1f42
nyeperson <jepgernale@gmail.com>
2020-06-22 20:58:15 +0800

    [UPDATE GMAIL EMAIL] dbstruct link and mail config helper

--- application/helpers/mail_config_helper.php
+++ application/helpers/mail_config_helper.php
@@ -22,29 +22,28 @@ function mail_config()
         //         'allow_self_signed' => true
         //     )
         // );
-
-        // $mail->SMTPDebug   = 2;
-        // $mail->Debugoutput = 'html';
-        // $mail->Mailer      = "smtp";
-        // $mail->SMTPAuth   = true;  
-        // $mail->Username   = 'wisdompark.web@gmail.com';  
-        // $mail->Password   = 'wisdompark00123';  
-        // $mail->Port       = 587;       
-        // $mail->Host       = "smtp.gmail.com";
-        // $mail->SMTPSecure = 'tls';
-
-        // $mail->From       = 'wisdompark.web@gmail.com';
-        // $mail->FromName   = 'WisdomPark';
-        
         $mail = new PHPMailer;
         $mail->isSMTP();
-        $mail->SMTPDebug = 0;
-        $mail->Host = 'smtp.hostinger.com';
-        $mail->Port = 587;
-        $mail->SMTPAuth = true;
-        $mail->Username = 'system@wisdomparkph.com';
-        $mail->Password = 'wisdompark00123';
-        $mail->setFrom('system@wisdomparkph.com', 'Wisdom Park');
+        $mail->SMTPDebug   = 0;
+        $mail->Debugoutput = 'html';
+        $mail->Mailer      = "smtp";
+        $mail->SMTPAuth   = true;  
+        $mail->Username   = 'wisdompark.web@gmail.com';  
+        $mail->Password   = 'wisdompark00123';  
+        $mail->Port       = 465;       
+        $mail->Host       = "ssl://smtp.gmail.com";
+        $mail->From       = 'wisdompark.web@gmail.com';
+        $mail->FromName   = 'WisdomPark';
+        
+        // $mail = new PHPMailer;
+        // $mail->isSMTP();
+        // $mail->SMTPDebug = 2;
+        // $mail->Host = 'smtp.hostinger.com';
+        // $mail->Port = 587;
+        // $mail->SMTPAuth = true;
+        // $mail->Username = 'system@wisdomparkph.com';
+        // $mail->Password = 'wisdompark00123';
+        // $mail->setFrom('system@wisdomparkph.com', 'Wisdom Park');
 
         return $mail;
     }
