ccffe92062adac8d820883d8a6c0e3c9b1d0aaa1
nyeperson <jepgernale@gmail.com>
2020-06-22 21:58:14 +0800

    update model add email column n retrieve

--- application/models/EventAttendance.php
+++ application/models/EventAttendance.php
@@ -19,7 +19,7 @@ class EventAttendance extends CI_Model {
     public function get_event_attendance()
     {
 
-        $this->db->select('SUM(a.head_count) as total, b.title,b.description,b.event_date,b.image');
+        $this->db->select('SUM(a.head_count) as total, b.title,b.description,b.event_date,b.image, a.email');
         $this->db->from('event_attendance a');
         $this->db->join('events b','b.id = a.event_id', 'left');
         $this->db->group_by('a.event_id');
