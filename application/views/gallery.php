7f23dc722370d077a949fd0d8e67a6f9e3734f2b
unknown <jepgernale@gmail.com>
2020-07-12 12:20:39 +0800

    song change controls test8

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -114,10 +114,10 @@
             foreach($videos as $videos_val)
             {
               echo "<div class='item videos col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-4'>
-                      <a href='https://www.youtube.com/embed/{$videos_val['image_name']}?playlist=tgbNymZ7vqY&loop=1' class='item-wrap fancybox' data-fancybox='gallery2'>
+                      <a href='https://www.youtube.com/embed/{$videos_val['image_name']}?' class='item-wrap fancybox' data-fancybox='gallery2'>
                       <span class='icon-search2'></span>
                       <iframe width='420' height='315' class='img-fluid'
-                      src='https://www.youtube.com/embed/{$videos_val['image_name']}?playlist=tgbNymZ7vqY&loop=1'>
+                      src='https://www.youtube.com/embed/{$videos_val['image_name']}?'>
                       </iframe>
                       </a>
                   </div>";
